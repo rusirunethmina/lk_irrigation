c2f22755ff189bcd2a361eebb790c385bd0ce221
nuuuwan <nuuuwan@gmail.com>
2025-11-30 12:53:25 +0530

    Refactor import statements for consistency and readability in RiverWaterLevelData.py

--- src/lk_irrigation/rwld/RiverWaterLevelData.py
+++ src/lk_irrigation/rwld/RiverWaterLevelData.py
@@ -5,10 +5,12 @@ from utils import Log
 
 from lk_irrigation.base.HasTimeMixin import HasTimeMixin
 from lk_irrigation.core.Station import Station
-from lk_irrigation.rwld.RiverWaterLevelDataFileMixin import \
-    RiverWaterLevelDataFileMixin
-from lk_irrigation.rwld.RiverWaterLevelDataLoadMixin import \
-    RiverWaterLevelDataLoadMixin
+from lk_irrigation.rwld.RiverWaterLevelDataFileMixin import (
+    RiverWaterLevelDataFileMixin,
+)
+from lk_irrigation.rwld.RiverWaterLevelDataLoadMixin import (
+    RiverWaterLevelDataLoadMixin,
+)
 
 log = Log("RiverWaterLevelData")
 
@@ -17,7 +19,6 @@ log = Log("RiverWaterLevelData")
 class RiverWaterLevelData(
     HasTimeMixin, RiverWaterLevelDataLoadMixin, RiverWaterLevelDataFileMixin
 ):
-
     station_name: str
     time_ut: int
     water_level_m: float
